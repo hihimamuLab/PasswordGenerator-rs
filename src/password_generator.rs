686e02abe190832154fd3878ed1dd31e10666b09
hihimamuLab <hihimamug14@gmail.com>
2024-09-21 22:58:41 +0900

    ADD: PasswordGenerator struct and dependencies, and add new()

--- src/password_generator.rs
+++ src/password_generator.rs
@@ -1,15 +1,42 @@
 /* ************************************************************************************************************************************************ */
 /*                                                                                                                                                  */
 /*                                                                                           :::   :::        :::          :::   :::     :::    ::: */
-/* main.rs                                                                                 :+:+: :+:+:      :+: :+:      :+:+: :+:+:    :+:    :+:  */
+/* password_generator.rs                                                                   :+:+: :+:+:      :+: :+:      :+:+: :+:+:    :+:    :+:  */
 /*                                                                                       +:+ +:+:+ +:+    +:+   +:+    +:+ +:+:+ +:+   +:+    +:+   */
 /* By: hihimamu <hihimamu@gmail.com>                                                    +#+  +:+  +#+   +#++:++#++:   +#+  +:+  +#+   +#+    +:+    */
 /*                                                                                     +#+       +#+   +#+     +#+   +#+       +#+   +#+    +#+     */
-/* Created: 2024/09/21 15:18:07 by hihimamu                                           #+#       #+#   #+#     #+#   #+#       #+#   #+#    #+#      */
-/* Updated: 2024/09/21 15:18:15 by hihimamu                                          ###       ###   ###     ###   ###       ###    ########.       */
+/* Created: 2024/09/21 22:39:06 by hihimamu                                           #+#       #+#   #+#     #+#   #+#       #+#   #+#    #+#      */
+/* Updated: 2024/09/21 22:57:38 by hihimamu                                          ###       ###   ###     ###   ###       ###    ########.       */
 /*                                                                                                                                                  */
 /* ************************************************************************************************************************************************ */
 
-mod password_generator;
+pub struct PasswordGenerator {
+    pub pool: PasswordGeneratorPool,
+    pub method: PasswordGeneratorMethod,
+    pub disable_str: String,
+    pub length: u8,
+}
 
-fn main() {}
+pub enum PasswordGeneratorMethod  {
+    SHA2512,
+    SHA3512,
+    BLAKE3,
+    Whirlpool
+}
+
+pub enum PasswordGeneratorPool {
+    a_Z(String),
+    a_Z0_9(String),
+    a_Z0_9symbol(String)
+}
+
+impl PasswordGenerator {
+    fn new() -> Self {
+        Self {
+            pool: PasswordGeneratorPool::a_Z("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string()),
+            method: PasswordGeneratorMethod::SHA2512,
+            disable_str: "".to_string(),
+            length: 8,
+        }
+    }
+}

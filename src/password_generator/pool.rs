9dcb9ab7c40181b11b569b8de3c82d190445a721
hihimamuLab <hihimamug14@gmail.com>
2024-09-23 10:36:57 +0900

    Implement BitOr trait for Pool struct

--- src/password_generator/pool.rs
+++ src/password_generator/pool.rs
@@ -6,7 +6,7 @@
 /* By: hihimamu <hihimamu@gmail.com>                                                    +#+  +:+  +#+   +#++:++#++:   +#+  +:+  +#+   +#+    +:+    */
 /*                                                                                     +#+       +#+   +#+     +#+   +#+       +#+   +#+    +#+     */
 /* Created: 2024/09/23 10:34:21 by hihimamu                                           #+#       #+#   #+#     #+#   #+#       #+#   #+#    #+#      */
-/* Updated: 2024/09/23 10:34:24 by hihimamu                                          ###       ###   ###     ###   ###       ###    ########.       */
+/* Updated: 2024/09/23 10:36:36 by hihimamu                                          ###       ###   ###     ###   ###       ###    ########.       */
 /*                                                                                                                                                  */
 /* ************************************************************************************************************************************************ */
 
@@ -20,4 +20,13 @@ impl Pool {
     pub const UPPERCASE: Self = Pool { value: 1  };
     pub const LOWERCASE: Self = Pool { value: 2  };
     pub const SYMBOL: Self = Pool { value: 4  };
+}
+
+impl BitOr for Pool {
+    type Output = Self;
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Self {
+            value: self.value | rhs.value
+        }
+    }
 }
